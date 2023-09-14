31eba1844f577284f407fc70735d599762eeda0d
Jumaiza <zaid.abujumaiza@gmail.com>
2023-09-13 23:40:20 -0400

    add health check endpoint

---
+++ express-server/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const port = 4000;
+const app = express();
+
+app.get('/health/check', (req,res) =>{
+    res.json({testString: "Hello World From Server!"});
+})
+
+app.listen(port, () => console.log(`Server is successfully listening on port ${port}`));
