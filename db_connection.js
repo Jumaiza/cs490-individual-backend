664780339a34070e3204811939ea2bdf41ba170b
Jumaiza <zaid.abujumaiza@gmail.com>
2023-09-25 17:04:42 -0400

    connected to db and organized repo

---
+++ db_connection.js
@@ -0,0 +1,14 @@
+var mysql = require('mysql');
+
+var conn = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "Cs288005!"
+});
+
+conn.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected to MySQL!");
+});
+
+module.exports = conn;
