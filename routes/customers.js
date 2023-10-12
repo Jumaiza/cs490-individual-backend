765bdd0eaaed2392043c022a433ae6b7dbe5501e
Jumaiza <zaid.abujumaiza@gmail.com>
2023-10-11 20:00:57 -0400

    added endpoint for deleting customer

--- routes/customers.js
+++ routes/customers.js
@@ -45,11 +45,12 @@ router.post('/customers/search-by-name', (req, res) => {
 
 router.post('/customers/rented-movies-by-id', (req, res) => {
     const customerId = req.body.customer_id;
-    const query = `SELECT film.title, film.rental_duration
-        from rental
-        inner join inventory on rental.inventory_id = inventory.inventory_id
-        inner join film on inventory.film_id = film.film_id
-        where rental.customer_id = ${customerId}`;
+    const query = `SELECT film.title,
+        CASE WHEN rental.return_date IS NOT NULL THEN 'RETURNED' ELSE 'NOT RETURNED YET' END AS return_status
+        FROM rental
+        INNER JOIN inventory ON rental.inventory_id = inventory.inventory_id
+        INNER JOIN film ON inventory.film_id = film.film_id
+        WHERE rental.customer_id = ${customerId}`;
     db_conn.query(query, (error, result) => {
         if(error){
             console.error(error)
@@ -57,6 +58,26 @@ router.post('/customers/rented-movies-by-id', (req, res) => {
         }
         res.json(result);
     });
+});
+
+router.post('/customers/delete-customer-by-id', (req, res) => {
+    const customerId = req.body.customer_id;
+    const addressId = req.body.address_id
+    const queries = [
+        `delete from rental WHERE customer_id = ${customerId};`,
+        `delete from payment WHERE customer_id = ${customerId};`,
+        `delete from customer WHERE customer_id = ${customerId};`,
+        `delete from address WHERE address_id = ${addressId};`,
+    ];
+    for(const query of queries){
+        db_conn.query(query, (error, result) => {
+            if(error){
+                console.error(error)
+                return res.status(500).json('Invalid Customer ID');
+            }
+        });
+    }
+    res.status(200).send('Customer deleted successfully!');
 });
 
 module.exports = router;
