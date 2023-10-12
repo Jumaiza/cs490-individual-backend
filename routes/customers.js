e03362f3d9fb3f6a6bed36e3f70adf99f33b543a
Jumaiza <zaid.abujumaiza@gmail.com>
2023-10-12 01:25:28 -0400

    added endpoint for adding new customer

--- routes/customers.js
+++ routes/customers.js
@@ -3,7 +3,9 @@ const router = express.Router();
 const db_conn = require('../db_connection');
 
 router.get('/customers/all-customers', (req, res) => {
-    const query = 'SELECT * FROM customer';
+    const query = `SELECT customer.address_id,customer_id,first_name,last_name,email,address,city,district,postal_code,phone FROM customer
+        inner join address on customer.address_id = address.address_id
+        inner join city on address.city_id = city.city_id`;
     db_conn.query(query, (error, result) => {
         if(error){
             console.error(error)
@@ -18,7 +20,10 @@ router.post('/customers/search-by-id', (req, res) => {
     if (customerId === ''){
         return res.json([]);
     }
-    const query = `SELECT * FROM customer WHERE customer_id = ${customerId}`
+    const query = `SELECT customer.address_id,customer_id,first_name,last_name,email,address,city,district,postal_code,phone FROM customer
+        inner join address on customer.address_id = address.address_id
+        inner join city on address.city_id = city.city_id
+        where customer_id = ${customerId}`;
     db_conn.query(query, (error, result) => {
         if(error){
             console.error(error)
@@ -33,7 +38,10 @@ router.post('/customers/search-by-name', (req, res) => {
     if (name === ''){
         return res.json([]);
     }
-    const query = `SELECT * FROM customer WHERE first_name LIKE "%${name}%" or last_name LIKE "%${name}%"`;
+    const query = `SELECT customer.address_id,customer_id,first_name,last_name,email,address,city,district,postal_code,phone FROM customer
+        inner join address on customer.address_id = address.address_id
+        inner join city on address.city_id = city.city_id
+        WHERE first_name LIKE "%${name}%" or last_name LIKE "%${name}%"`;
     db_conn.query(query, (error, result) => {
         if(error){
             console.error(error)
@@ -63,6 +71,7 @@ router.post('/customers/rented-movies-by-id', (req, res) => {
 router.post('/customers/delete-customer-by-id', (req, res) => {
     const customerId = req.body.customer_id;
     const addressId = req.body.address_id
+    console.log(addressId)
     const queries = [
         `delete from rental WHERE customer_id = ${customerId};`,
         `delete from payment WHERE customer_id = ${customerId};`,
@@ -79,5 +88,41 @@ router.post('/customers/delete-customer-by-id', (req, res) => {
     }
     res.status(200).send('Customer deleted successfully!');
 });
+
+router.put('/customers/add-customer', (req, res) => {
+    const customer = req.body;
+
+    let cityId;
+    const cityQuery = `INSERT INTO city (city, country_id) VALUES("${customer.city}", 103);`;
+
+    db_conn.query(cityQuery, (error, result) => {
+        if (error) {
+            console.error(error);
+            return res.status(500).json({ error: 'Error adding city' });
+        }
+        cityId = result.insertId;
+
+        const addressQuery = `INSERT INTO address (address, address2, district, city_id, postal_code, phone, location)
+        VALUES ("${customer.address}", NULL, "${customer.district}",${cityId}, "${customer.postal_code}","${customer.phone}", POINT(0,0));`;
+        db_conn.query(addressQuery, (error, result) => {
+            if (error) {
+                console.error(error);
+                return res.status(500).json({ error: 'Error adding address' });
+            }
+            const addressId = result.insertId;
+
+            const customerQuery = `INSERT INTO customer (store_id, first_name, last_name, email, address_id, active, create_date)
+            VALUES (1, "${customer.first_name}", "${customer.last_name}", "${customer.email}", ${addressId}, 1, CURRENT_TIMESTAMP);`;
+            db_conn.query(customerQuery, (error, result) => {
+                if (error) {
+                    console.error(error);
+                    return res.status(500).json({ error: 'Error adding customer' });
+                }
+                res.status(200).send(`New Customer with ID: ${result.insertId} added successfully!`);
+            });
+        });
+    });
+});
+
 
 module.exports = router;
